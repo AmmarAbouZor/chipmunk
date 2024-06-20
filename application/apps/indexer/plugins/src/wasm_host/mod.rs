0d0abf97d75abfbcef89a7afa8fc12ef1f4b1623
Ammar Abou Zor <ammar.abou.zor@accenture.com>
2024-06-20 11:50:39 +0200

    Plugins: Initialize WASM Engine

--- application/apps/indexer/plugins/src/wasm_host/mod.rs
+++ application/apps/indexer/plugins/src/wasm_host/mod.rs
@@ -1,4 +1,40 @@
 // TODO: Temporally place holder
 #![allow(dead_code, unused_imports, unused)]
 
-pub struct WasmHose {}
+use thiserror::Error;
+use tokio::sync::OnceCell;
+use wasmtime::{Config, Engine};
+
+#[derive(Debug, Error)]
+pub enum WasmInitError {
+    #[error("Error while initializing WASM Engine")]
+    EngineError(#[from] anyhow::Error),
+}
+
+pub struct WasmHost {
+    engine: Engine,
+}
+
+impl WasmHost {
+    fn init() -> Result<Self, WasmInitError> {
+        let mut config = Config::new();
+        config.wasm_component_model(true);
+        config.async_support(true);
+
+        //TODO AAZ: Check the impact of these when we have a proper benchmarks
+        // config.relaxed_simd_deterministic(true);
+        // config.cranelift_opt_level(wasmtime::OptLevel::Speed);
+
+        let engine = Engine::new(&config)?;
+
+        let host = Self { engine };
+
+        Ok(host)
+    }
+}
+
+pub async fn get_wasm_host() -> &'static Result<WasmHost, WasmInitError> {
+    static WASM_HOST: OnceCell<Result<WasmHost, WasmInitError>> = OnceCell::const_new();
+
+    WASM_HOST.get_or_init(|| async { WasmHost::init() }).await
+}
